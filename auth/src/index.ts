73d44f24869a7689b054b851bafd79bff425a19c
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-09 17:06:03 -0300

    Accessing Env Variables in a Pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,6 +34,10 @@ app.all("*", () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to MongoDB");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -42,7 +42,7 @@ router.post(
         id: user.id,
         email: user.email,
       },
-      "asdf"
+      process.env.JWT_KEY!
     );
 
     // Store it on session object
