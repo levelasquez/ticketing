67f383993c95067167dae65aaf3d815340955dd2
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-10 10:28:35 -0300

    Testing Invalid Input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -8,3 +8,31 @@ it("returns a 201 on successful signup", async () => {
     .send({ email: "test@test.com", password: "password" })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "asdfghjkl", password: "password" })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "p" })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ password: "password" })
+    .expect(400);
+
+  return request(app).post("/api/users/signup").send({}).expect(400);
+});
