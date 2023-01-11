fc20015343de478183484685afe33b5f39fafe58
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-11 09:37:45 -0300

    Successful Account Signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
-    console.log(email, password);
+    const response = await axios.post("/api/users/signup", { email, password });
+
+    console.log(response.data);
   };
 
   return (
