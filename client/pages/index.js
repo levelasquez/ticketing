7b7423212194bf809a6622f677bbb4d5fa2b244d
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-11 10:22:20 -0300

    Overview on Server Side Rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
-const Landing = () => {
+const Landing = ({ color }) => {
+  console.log("I am in the component", color);
+
   return <h1>Landing Page</h1>;
+};
+
+Landing.getInitialProps = () => {
+  console.log("I am on the server!");
+
+  return { color: "red" };
 };
 
 export default Landing;
