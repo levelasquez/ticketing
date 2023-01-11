5185248694d26ad8141986eb59889857b08f65e4
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-11 10:10:34 -0300

    An onSuccess Callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 
 import useRequest from "../../hooks/use-request";
 
@@ -10,9 +10,10 @@ const Signup = () => {
     url: "/api/users/signup",
     method: "post",
     body: { email, password },
+    onSuccess: () => Router.push("/"),
   });
 
-  const onSubmit = async (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
 
     doRequest();
