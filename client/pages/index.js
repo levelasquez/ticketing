4e9d936337fc525c3c32bcebba3542ec693be6a6
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-12 18:57:56 -0300

    Passing Props Through

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,7 +9,7 @@ const Landing = ({ currentUser }) => {
 };
 
 Landing.getInitialProps = async (context) => {
-  console.log("LANDIN GPAGE!");
+  console.log("LANDING PAGE!");
   const client = buildClient(context);
 
   const { data } = await client.get("/api/users/currentuser");
