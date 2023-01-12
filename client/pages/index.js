0ec39d834611ffaee94b785efd1e3c8d6f343b75
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-12 10:28:28 -0300

    Passing Through the Cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,16 +9,14 @@ const Landing = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-Landing.getInitialProps = async () => {
+Landing.getInitialProps = async ({ req }) => {
   if (typeof window === "undefined") {
     // we are on the server!
     // request should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
     const { data } = await axios.get(
       "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
       {
-        headers: {
-          Host: "ticketing.dev",
-        },
+        headers: req.headers,
       }
     );
 
