10a5b27cae9240c7f552baa56c30e73ef2364408
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-12 10:23:57 -0300

    Specifying the Host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Landing = ({ currentUser }) => {
-  // console.log(currentUser);
+  console.log(currentUser);
   // axios.get("/api/users/currentuser").catch((err) => {
   //   console.log(err.message);
   // });
@@ -13,9 +13,22 @@ Landing.getInitialProps = async () => {
   if (typeof window === "undefined") {
     // we are on the server!
     // request should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return data;
   } else {
     // we are on the browser!
     // request can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
   }
 
   return {};
