fe09b771768b7582c5eeafd0ad557a473b45d279
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-12 17:24:21 -0300

    A Reusable Header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;
