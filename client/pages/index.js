1a823e641525493c5c09ddf44de7782057ec6456
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-12 10:36:20 -0300

    A Reusable API Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const Landing = ({ currentUser }) => {
   console.log(currentUser);
@@ -9,27 +9,12 @@ const Landing = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-Landing.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server!
-    // request should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
+Landing.getInitialProps = async (context) => {
+  const client = buildClient(context);
 
-    return data;
-  } else {
-    // we are on the browser!
-    // request can be made with a base url of ''
-    const { data } = await axios.get("/api/users/currentuser");
+  const { data } = await client.get("/api/users/currentuser");
 
-    return data;
-  }
-
-  return {};
+  return data;
 };
 
 export default Landing;
