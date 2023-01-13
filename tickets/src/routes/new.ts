054c46f569e5fb18e25a6a5529b5fb46157b9a23
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-13 20:58:01 -0300

    Validating Title and Price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@lvtickets/common";
+import { requireAuth, validateRequest } from "@lvtickets/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/tickets", requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
