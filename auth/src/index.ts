c6c9f1d3c8691908ae8155599efe1f87e833c974
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-13 19:49:28 -0300

    Quick Auth Update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,13 @@ const start = async () => {
     throw new Error("JWT_KEY must be defined");
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
