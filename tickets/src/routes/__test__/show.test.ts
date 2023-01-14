5cd09c1ee65d9ba3893c5e753abb1ad866d37a40
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-14 11:53:28 -0300

    Better Error Logging

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,12 @@
 import request from "supertest";
+import mongoose from "mongoose";
 
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
-  const response = await request(app).get("/api/tickets/asd").send({});
+  const id = new mongoose.Types.ObjectId().toHexString();
 
-  console.log(response.body);
+  await request(app).get(`/api/tickets/${id}`).send({}).expect(404);
 });
 
 it("returns the tickets if the ticket is found", async () => {
