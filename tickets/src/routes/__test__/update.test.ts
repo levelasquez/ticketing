62981ffdab5f3586967cb2974948e7280def010a
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-14 13:19:48 -0300

    Ticket Updating

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import request from "supertest";
+
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("returns a 404 if the provided id does not exist", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set("Cookie", global.signin())
+    .send({ title: "title", price: 20 })
+    .expect(404);
+});
+
+it("returns a 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({ title: "title", price: 20 })
+    .expect(401);
+});
+
+it("returns a 404 if the user does not own the ticket", async () => {});
+
+it("returns a 400 if the user provides an invalid title or price", async () => {});
+
+it("updates the ticket provided valid inputs", async () => {});
