09bccb3ea644683534b01e6d9a73a78bfa61f05f
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-13 21:21:28 -0300

    Creation via Route Handler

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -50,11 +51,21 @@ it("returns an error if an invalid price is provided", async () => {
 });
 
 it("creates a ticket with valid inputs", async () => {
-  // add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+
+  expect(tickets.length).toEqual(0);
+
+  const title = "title";
 
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
-    .send({ title: "title", price: 20 })
+    .send({ title, price: 20 })
     .expect(201);
+
+  tickets = await Ticket.find({});
+
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@lvtickets/common";
 import { body } from "express-validator";
 
+import { Ticket } from "../models/ticket";
+
 const router = express.Router();
 
 router.post(
@@ -14,8 +16,18 @@ router.post(
       .withMessage("Price must be greater than 0"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
