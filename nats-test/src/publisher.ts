f649f542a2bbe5b2d4ce5c3d79ae0256310d1d34
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-17 10:30:46 -0300

    Publising Events

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,4 +6,14 @@ const stan = nats.connect("ticketing", "abc", {
 
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
+
+  const data = JSON.stringify({
+    id: "1",
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticked:created", data, () => {
+    console.log("Event published");
+  });
 });
