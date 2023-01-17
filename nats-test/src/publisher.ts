d24f221a18eac55d03750ed2d913bdd2fa426349
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-17 10:39:20 -0300

    Listening for Data

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from "node-nats-streaming";
 
+console.clear();
+
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
@@ -13,7 +15,7 @@ stan.on("connect", () => {
     price: 20,
   });
 
-  stan.publish("ticked:created", data, () => {
+  stan.publish("ticket:created", data, () => {
     console.log("Event published");
   });
 });
