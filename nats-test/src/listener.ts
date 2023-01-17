8120e9840c61abeaaab412151c97d92599ddce04
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-17 14:03:12 -0300

    Durable Subscriptions

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -18,10 +18,12 @@ stan.on("connect", () => {
   const options = stan
     .subscriptionOptions()
     .setManualAckMode(true)
-    .setDeliverAllAvailable();
+    .setDeliverAllAvailable()
+    .setDurableName("orders-service");
+
   const subscription = stan.subscribe(
     "ticket:created",
-    // "orders-service-queue-group",
+    "orders-service-queue-group",
     options
   );
 
