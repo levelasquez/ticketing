6464d9c295ba5a13c156db73f98b750694bb2db6
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-19 10:22:04 -0300

    Awaiting Event Publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -8,7 +8,7 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
   stan.on("close", () => {
@@ -18,11 +18,15 @@ stan.on("connect", () => {
 
   const publisher = new TicketCreatedPublisher(stan);
 
-  publisher.publish({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 process.on("SIGINT", () => stan.close());
