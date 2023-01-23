54e501fe8fb2a011b1ef58a11bbeb4ce1bc19ccb
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-23 10:31:17 -0300

    Asserting Tickets Exist

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import mongoose from "mongoose";
+
+import { app } from "../../app";
+
+it("returns an error if the ticket does not exist", async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it("returns an error if the ticket is already reserved", async () => {});
+
+it("reserves a ticket", async () => {});
