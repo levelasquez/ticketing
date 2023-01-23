bbbd16744524e846737abe9de8338ecba3450b20
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-23 19:38:51 -0300

    Cancelling an Order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,36 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@lvtickets/common";
 import express, { Request, Response } from "express";
+
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
