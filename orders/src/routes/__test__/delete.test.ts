6d0c9aba7a2b02e44a1507ec8773bac6734b2043
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-24 10:38:04 -0300

    Testing Event Publishing

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   // Create a ticket with Ticket model
@@ -34,4 +35,30 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits a order cancelled event");
+it("emits a order cancelled event", async () => {
+  // Create a ticket with Ticket model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  // Expectation to make sure the event is published
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
