113dd3e38fed37944438cdbd0eb541264193e35e
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-24 21:35:48 -0300

    A Few More Reminders

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,9 +1,11 @@
 import { Listener, Subjects, TicketCreatedEvent } from "@lvtickets/common";
 import { Message } from "node-nats-streaming";
 
+import { queueGroupName } from "./queue-group-name";
+
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
 }
