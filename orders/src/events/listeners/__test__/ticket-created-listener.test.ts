016ac4c4a5c00de5acf71ddbf61612fea86986a5
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-26 10:33:48 -0300

    Testing Listeners

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,15 @@
+const setup = async () => {
+  // Create an instance of the listener
+  // Create a fake data event
+  // Create a fake message object
+};
+
+it("creates and saves a ticket", async () => {
+  // Call the onMessage function with the data object + message object
+  // Write assertions to make sure a ticket was created!
+});
+
+it("acks the message", async () => {
+  // Call the onMessage function with the data object + message object
+  // Write assertions to make sure ack function is called
+});
