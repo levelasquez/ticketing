8660e5dbec2628baff5187d210986bb00d3f2583
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-27 20:08:49 -0300

    Reserving a Ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,29 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@lvtickets/common";
 import { Message } from "node-nats-streaming";
 
+import { Ticket } from "../../models/ticket";
 import { queueGroupName } from "./queue-group-name";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+
+    // Ack the message
+    msg.ack();
+  }
 }
