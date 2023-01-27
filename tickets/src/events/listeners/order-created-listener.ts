d35736a23505497dad681451fdd7a3f78106cd71
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-27 19:55:30 -0300

    Building the Listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,11 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@lvtickets/common";
+import { Message } from "node-nats-streaming";
+
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
