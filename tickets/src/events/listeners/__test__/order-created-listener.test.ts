24d7f58a4895190c91de6bcc1caeae1dac336732
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-27 23:57:37 -0300

    Test Implementation

--- tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -38,3 +38,21 @@ const setup = async () => {
 
   return { listener, ticket, data, msg };
 };
+
+it("sets the orderId of the ticket", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
