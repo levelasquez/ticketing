29e7efe2c33c6a781be11992800f5fbb9b032aca
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-28 22:13:32 -0300

    Order Cancelled Listener

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,39 @@
+import { Listener, OrderCancelledEvent, Subjects } from "@lvtickets/common";
+import { Message } from "node-nats-streaming";
+
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: undefined });
+
+    // Save the ticket
+    await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      version: ticket.version,
+      orderId: ticket.orderId,
+    });
+
+    // Ack the message
+    msg.ack();
+  }
+}
