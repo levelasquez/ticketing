80f90e390941d3d7f38a679dc3347702ef03f38c
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-29 13:44:43 -0300

    Testing the Expiration Complete Listener

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { ExpirationCompleteEvent } from "@lvtickets/common";
+
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Order, OrderStatus } from "../../../models/order";
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "123",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, ticket, data, msg };
+};
