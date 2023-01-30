7950265b064d423d4b573c0fe1b969e68a0dc072
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-30 10:34:22 -0300

    Update-If-Current

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { OrderStatus } from "@lvtickets/common";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface OrderAttrs {
   id: string;
@@ -45,6 +46,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
