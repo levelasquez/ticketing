d4061d0b257e9536180472f732c9fa77dd040a01
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-01-31 12:01:45 -0300

    Implementing the Create Charge Handler

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@lvtickets/common";
 
+import { createChargeRouter } from "./routes/new";
+
 const app = express();
 app.set("trust proxy", true);
 
@@ -15,6 +17,8 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
---
+++ payments/src/routes/new.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@lvtickets/common";
+
+const router = express.Router();
+
+router.post(
+  "/api/payments",
+  requireAuth,
+  [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({ success: true });
+  }
+);
+
+export { router as createChargeRouter };
