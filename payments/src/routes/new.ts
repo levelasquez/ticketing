4483ae976c5870780c3f9fdbce0c055fb9951060
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-02-01 09:43:00 -0300

    Validating Order Payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,15 @@
-import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@lvtickets/common";
+import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@lvtickets/common";
+
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -10,6 +19,22 @@ router.post(
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for an cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
