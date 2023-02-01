2e0126259bf150daa427ae5788da144b7e0c3ed0
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-02-01 09:50:20 -0300

    Testing Order Validation Before Payment

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { OrderStatus } from "@lvtickets/common";
+
+import { app } from "../../app";
+import { Order } from "../../models/order";
+
+it("returns a 404 when pucharsing an order that does not exists", async () => {
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "asd",
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it("returns a 401 when pucharsing an order that doesnt belong to the user", async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 20,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "asd",
+      orderId: order.id,
+    })
+    .expect(401);
+});
+
+it("returns a 400 when pucharsing a cancelled order", async () => {});
