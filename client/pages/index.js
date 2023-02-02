cc01a12f8b26f7c5e6db29e6cfa5c2e84671997b
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-02-02 13:21:52 -0300

    Listing All Tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,33 @@
-const Landing = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const Landing = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
 Landing.getInitialProps = async (context, client, currentUser) => {
-  return {};
+  const { data } = await client.get("/api/tickets");
+
+  return { tickets: data };
 };
 
 export default Landing;
